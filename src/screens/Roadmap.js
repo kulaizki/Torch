c0c9607df201a3d3b3048f4e51752fb5d28d5776
kulaizki <kulaizke@gmail.com>
2024-04-29 23:22:54 +0800

    feat: add base roadmap screen

---
+++ src/screens/Roadmap.js
@@ -0,0 +1,26 @@
+import { View, Image, Text } from 'react-native';
+import { GlobalStyles } from '../styles/GlobalStyles';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const log = () => {
+    console.log('Button pressed');
+}
+
+export default function Roadmap() {
+    return (
+        <SafeAreaView style={GlobalStyles.centerView}>
+        <View style={[GlobalStyles.centerView, styles.explore]}>
+            <Text className='font-[OpenSans-Bold] text-5xl color-white'>Roadmap</Text>
+        </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = {
+    roadmap: {
+        backgroundColor: '#212121',
+        width: '100%',
+        gap: 100,
+    },
+};
+
