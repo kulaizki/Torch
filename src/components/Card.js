6a06e26696e6693cd2e0ab6da2294f9469504df0
kulaizki <kulaizke@gmail.com>
2024-04-30 00:52:12 +0800

    feat: refactor nativewind

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,12 +9,13 @@ export const Card = ({ title, description, img, onPress }) => {
       paddingHorizontal: 12,
       paddingVertical: 20,
       borderRadius: 8, 
+      gap: 8,
     },
   });
 
   return (
     <TouchableOpacity onPress={onPress} style={styles.card} className=''>
-      <Image source={img} className='w-[120px] h-[120px]' />
+      <Image source={img} className='w-[120px] h-[120px] rounded-md' />
       <Text className='text-2xl color-black font-[OpenSans-SemiBold]'>{title}</Text>
       <Text className='color-black font-[OpenSans-SemiBold]'>{description}</Text>
     </TouchableOpacity>
