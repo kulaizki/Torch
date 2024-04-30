60d10a6c63845bd0526d61d27d97e67a66a7194f
kulaizki <kulaizke@gmail.com>
2024-04-30 20:50:57 +0800

    style: add subtext color

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,10 +14,10 @@ export const Card = ({ title, description, img, onPress }) => {
   });
 
   return (
-    <TouchableOpacity onPress={onPress} style={styles.card} className=''>
+    <TouchableOpacity onPress={onPress} style={styles.card}>
       <Image source={img} className='w-[120px] h-[120px] rounded-md' />
       <Text className='text-2xl color-white font-[OpenSans-SemiBold]'>{title}</Text>
-      <Text className='color-white font-[OpenSans-SemiBold]'>{description}</Text>
+      <Text className='color-[#d2d2d2] font-[OpenSans-SemiBold]'>{description}</Text>
     </TouchableOpacity>
   );
 };
