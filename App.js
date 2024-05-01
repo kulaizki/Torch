f8fcf98395d297aa36693cfa7ac7b8b13f97ab38
kulaizki <kulaizke@gmail.com>
2024-05-01 16:10:21 +0800

    feat: add roadmap screen

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Landing" component={Landing}/>
         <Stack.Screen name="Explore" component={Explore}/>
+        <Stack.Screen name="Roadmap" component={Explore}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
