a34482747a7957c204a01e3efbe2ace804cf88b8
kulaizki <kulaizke@gmail.com>
2024-05-05 04:40:00 +0800

    feat: map cards to uniquely navigate

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -4,6 +4,12 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Card } from '../components/Card';
 import { Header } from '../components/Header';
 
+const cards = [ 
+    { title: 'Javascript Developer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien fermentum', img: require('../assets/images/js.png') },
+    { title: 'Python Developer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien fermentum', img: require('../assets/images/python.png') },
+    { title: 'React Developer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien fermentum', img: require('../assets/images/react.webp') },
+  ];
+
 const log = () => {
     console.log('Button pressed');
 }
@@ -14,26 +20,17 @@ export default function Explore({ navigation }) {
             <View style={[GlobalStyles.centerView, styles.explore]}>
                 <Header className='absolute'/>
                 <ScrollView style={styles.cardContainer}>
-                    <Text className='font-[OpenSans-Bold] text-5xl color-white pt-8 pb-4' >Explore</Text>
-                    <Text className='font-[OpenSans-Regular] text-2xl color-[#d2d2d2] pb-8' >Choose your path</Text>
-                    <Card 
-                        title={'Javascript Developer'} 
-                        description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.'}
-                        img={require('../assets/images/js.png')}
-                        onPress={log}
-                    />
-                    <Card 
-                        title={'Python Developer'} 
-                        description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.'}
-                        img={require('../assets/images/python.png')}
-                        onPress={log}
-                    />
-                    <Card 
-                        title={'React Developer'} 
-                        description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.'}
-                        img={require('../assets/images/react.webp')}
-                        onPress={log}
-                    />
+                    <Text className='font-[OpenSans-Bold] text-5xl color-white pt-16 pb-4' >Explore</Text>
+                    <Text className='font-[OpenSans-Light] text-2xl color-[#d2d2d2] pb-8' >Choose your path.</Text>
+                    {cards.map((card) => (
+                        <Card
+                        key={card.title} 
+                        title={card.title}
+                        description={card.description}
+                        img={card.img}
+                        onPress={() => navigation.navigate('Roadmap', card)} 
+                        />
+                    ))}
                 </ScrollView>
             </View>
         </SafeAreaView>
@@ -47,7 +44,6 @@ const styles = {
     },
     cardContainer: {
         width: '100%',
-        paddingLeft: 24,
-        paddingRight: 24, 
+        paddingHorizontal: 24,
     },
 };
