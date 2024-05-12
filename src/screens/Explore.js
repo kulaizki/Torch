67e1bf829f56827b0f41b04c3e55f2894bfb5201
kulaizki <kulaizke@gmail.com>
2024-05-12 13:45:30 +0800

    feat: add game dev & cybersec engineer to cards

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -8,6 +8,8 @@ const cards = [
     { title: 'Javascript Developer', description: 'Master the fundamentals then delve into frameworks and libraries to become a well-rounded JavaScript developer.', img: require('../assets/images/js.png') },
     { title: 'Python Developer', description: 'Build a strong foundation in Python syntax, data structures, and algorithms before exploring specialized libraries and frameworks for your desired development path.', img: require('../assets/images/python.png') },
     { title: 'React Developer', description: 'Grasp core React concepts, hooks, and state management, then conquer popular libraries and tools to become a proficient React developer.', img: require('../assets/images/react.webp') },
+    { title: 'Game Developer', description: 'description', img: require('../assets/images/python.png') },
+    { title: 'Cybersec Engineer', description: 'description', img: require('../assets/images/python.png') },
   ];
 
 export default function Explore({ navigation }) {
