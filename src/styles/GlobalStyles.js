225d381a84585f60c9bc1f4eb79731abe50f436c
kulaizki <kulaizke@gmail.com>
2024-05-18 01:59:42 +0800

    style: darken dark2

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const Colors = {
   dark1: '#212121',
-  dark2: '#414141',
+  dark2: '#353535',
   dark3: '#141414',
   subtext: '#9E9E9E',
   orange: '#FFA800',
